7639b9883d153afae33c95c1293972969201c798
RWangjj <93642516+RWangjj@users.noreply.github.com>
2022-08-13 13:50:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 # 公众号配置
 # 公众号appId
-app_id = ""
+app_id = "wx4b6e91bc021f23"
 # 公众号appSecret
-app_secret = ""
+app_secret = "f55925c57ecf300f119b5bb128cfac4e"
 # 模板消息id
-template_id = ""
+template_id = "up7hAKOKZJUuDRZqWT0NBguLQ7K8nUyAAvVQjQcUs8"
 # 接收公众号消息的微信号
-user = ""
+user = "oughs5mf_f7Uk8xlX9FJ663L3jgY"
 
 # 信息配置
 # 所在省份
-province = "广东"
+province = "湖北"
 # 所在城市
-city = "广州"
+city = "武汉"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = "1997-1-1"
+birthday = "2002-7-27"
 # 在一起的日子，格式同上
-love_date = "2022-1-1"
+love_date = "2021-11-10"
 
